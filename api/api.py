48ac8c0bb7a6d0aa8b02bcc067a49644837a3c58
Robotboyx <vuthy.v@ligercambodia.org>
2021-02-14 23:59:08 +0700

    restructure app

--- api/api.py
+++ api/api.py
@@ -1,8 +1,31 @@
 import time
-from flask import Flask
+from flask import Flask, render_template,send_from_directory,request, jsonify, make_response
+# from flask_cors import CORS, cross_origin
+# import boto3
+# import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')    
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/api')
+# @cross_origin()
+def Welcome():
+    return "Welcome to the API!!!"
+
+# @app.route('/api/justpie/')
+# @cross_origin()
+# def GeneratePie():
+#     ....
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')   
